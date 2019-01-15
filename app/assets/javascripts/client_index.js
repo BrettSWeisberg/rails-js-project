b54fdd1afe4839861c2cbd891736eda08b833fd6
github username <github email address>
2019-01-15 17:23:14 -0500

    Index view update

--- app/assets/javascripts/client_index.js
+++ app/assets/javascripts/client_index.js
@@ -19,12 +19,12 @@ function sortClients(){
       success: function (response) {
 
       response.sort(function(a, b){
-        var x = a.name.toLowerCase();
-        var y = b.name.toLowerCase();
+        let x = a.name.toLowerCase();
+        let y = b.name.toLowerCase();
         if (x < y) {return -1;}
         if (x > y) {return 1;}
         return 0;
-      }); //clients_list
+      });
 
       for (i in response) {
       document.getElementById(`clients_list`).innerHTML += "<li>" + response[i].name + "</li>";
