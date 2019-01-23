eb2539a0a6952bae18681a409af0bb355ed0d008
github username <github email address>
2019-01-22 20:19:40 -0500

    Update to delete route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,14 @@ class SessionsController < ApplicationController
 
 
     def create
+      
       if auth_hash = request.env['omniauth.auth'] # log in omniauth
+
          manager = Manager.find_or_create_by_omniauth(auth_hash)
          session[:manager_id] = manager.id
          redirect_to manager_path(manager)
       else
+
         @manager = Manager.find_by(email: params[:session][:email].downcase) #bcrypt login
           if @manager && @manager.authenticate(params[:session][:password])
               session[:manager_id] = @manager.id
